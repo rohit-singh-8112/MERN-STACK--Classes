476ccf9d40a45a37c2e758e1c7900800b98bcab2
Rohit Chauhan <chauhanrc7878@gmail.com>
2025-12-24 16:24:57 +0530

    add Portfolio

---
+++ practice/Portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {  return (
+    <>
+      <div className="App container text-red-500 text-3xl font-bold text-center">
+        <h1>Welcome to My Portfolio</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
